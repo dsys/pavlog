74c38fe6731a0d78144fb4acf0ca7a1009264e1d
Alex Kern <alex@kern.io>
2015-12-28 19:44:38 -0800

    You must pass in a logger obj.

--- src/middleware.js
+++ src/middleware.js
@@ -3,10 +3,6 @@ import morgan from 'morgan'
 import through2 from 'through2'
 
 export default function (logger, format) {
-  if (typeof logger === 'string') {
-    logger = createLogger(logger)
-  }
-
   const stream = through2(function (output, enc, callback) {
     logger(output.toString('utf8').trim())
     this.push(output)
