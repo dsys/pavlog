695b1e3e7ab739875d025b8e9a1a517163b64045
Alex Kern <alex@kern.io>
2015-12-28 19:37:55 -0800

    Export middleware correctly.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import createLogger from './logger'
+import middleware from './middleware'
 
-export default createLogger()
-export { default as middleware } from './middleware'
+const rootLogger = createLogger()
+rootLogger.middleware = middleware
+export default rootLogger
